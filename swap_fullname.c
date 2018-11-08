f037cef91715f5ebf21d67da3e01601e46ee2143
saebs <sabelo.n@yandex.com>
2018-11-08 09:31:43 +0200

    swap names

--- swap_fullname.c
+++ swap_fullname.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main(void)
+
+int main(void) 
 {
+    int n, i;
+    char first[101], last[101];
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+       scanf("%s %s", first , last);
+       printf("%s %s\n", last, first);
+    }
+
     return 0;
 }
