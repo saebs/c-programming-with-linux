1ceefe4202c55a3d48038c835ab208d924a30f35
saebs <sabelo.n@yandex.com>
2018-11-08 10:39:10 +0200

    longest word

---
+++ longest_word.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    char text[100];
+    int i = 0;
+    int longest = 0;
+    for (i = 0; i < n; i++){
+        scanf("%s", text);
+        int t = 0;
+        while (text[t] != '\0')
+            t++;
+        if (longest < t)
+            longest = t;
+    } 
+    printf("%d\n", longest);
+
+    return 0;
+}
