cb358dc6f5dc46183a5cb2e2afd32029ea7c7030
Sabelo Menziwa Ntabeni <ntabenism@gmail.com>
2019-01-04 09:49:29 +0000

    print text in reverse order

---
+++ unit-4 pointers and memory management/unit-4.2 multidimensional arrays/activity-printatextinreverseorder.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char text[68][40];
+    for (int i = 0; i < 68; i++)
+    {
+        scanf("%s", text[i]);
+    }
+    for (int j = 67; j > -1; j--)
+    {
+        printf("%s ", text[j]);
+    }
+    printf("\n");
+    return 0;
+
+}
+
+/*
+Science Computer on Papers Selected Knuth, Ervin Donald ― correct."
+be will results the that reader a convince to and works algorithm an way the communicate to concepts,
+mathematical as well as forms literary and aesthetic traditional with works who essayist an ideally is programmer A clearly.
+them understand can beings human that so and quickly them perform can machines computing that so written are programs best "The
+*/
