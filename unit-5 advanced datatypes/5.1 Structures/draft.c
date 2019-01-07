982619007c1bfdb2e3cff3ae52e4f74710969444
Sabelo Menziwa Ntabeni <ntabenism@gmail.com>
2019-01-07 15:03:49 +0000

    practise code

---
+++ unit-5 advanced datatypes/5.1 Structures/draft.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+struct point {
+    double x;
+    double y;
+    double z;
+};
+
+void get_plot(struct point *);
+void gPlot(struct point);
+void scale(struct point *);
+
+int main(void)
+{
+    struct point focal;
+    get_plot(&focal);
+    gPlot(focal);
+    scale(&focal);
+    gPlot(focal);
+    return 0;
+}
+
+void get_plot(struct point * location)
+{
+    printf("Enter coordinates, x y z: \n");
+    scanf("%lf %lf %lf", &location->x, &location->y, &location->z);
+}
+
+void gPlot(struct point pnt)
+{
+    printf("\n\n");
+    printf("\t\ty:%.3lf\n\t\t\t\t\tz:%.3lf\n\n\n\n\t\tx:%.3lf\n\n\n", pnt.y, pnt.z, pnt.x);
+}
+
+void scale(struct point * pointa)
+{
+    double fac;
+    printf("Enter scale factor: ");
+    scanf("%lf", &fac);
+    printf("...scaling...\n");
+    pointa->x  = pointa->x * fac;
+    pointa->y  = pointa->y * fac;
+    pointa->z  = pointa->z * fac;
+}
