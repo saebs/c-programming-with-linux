a2e48ba7f8064bf19caf9eda3bbe68d1b0c6b59a
Sabelo Menziwa Ntabeni <ntabenism@gmail.com>
2019-01-07 09:20:15 +0000

    defining structures and accessing them

---
+++ unit-5 advanced datatypes/5.1 Structures/definestructures.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+struct fplManager {
+    char name[40];
+    char country[40];
+    int weekPoints;
+    int rank;
+};
+
+void fplProfile(struct fplManager *);
+void readManager(struct fplManager *);
+
+int main(void)
+{
+    // struct fplManager one = {"Sabelo", 89, 3};
+    struct fplManager best;
+    readManager(&best);
+
+    fplProfile(&best);
+}
+
+void fplProfile(struct fplManager * mgr)
+{
+    printf("\n-FPL Manager Profile-\n");
+    // printf("Name: %s\n", mgr -> name);
+    printf("Name: %s\n", (*mgr).name); // dereferencing verbose method
+    printf("Country: %s\n", mgr->country); // automatic dereferencing
+    printf("Points: %d\n", mgr->weekPoints *12);
+    printf("Level: %d\n", mgr->rank);
+}
+
+void readManager(struct fplManager * mgr)
+{
+
+    printf("Please enter FPL manager name: ");
+    scanf("%s", mgr -> name);
+    printf("Please enter FPL manager country: ");
+    scanf("%s", mgr -> country);
+    printf("Please enter Weekly average score: " );
+    scanf("%d", &mgr -> weekPoints);
+    if (mgr->weekPoints > 80)
+     {
+         mgr->rank = 10;
+     } else {
+         mgr->rank = 1;
+     }
+
+}
